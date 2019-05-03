aac4969379b338f7e636f0f46b74c0f2cb55eb33
bcoggins78 <bcoggins78@gmail.com>
2019-05-02 20:40:33 -0400

    Added sample friend json data

---
+++ app/data/friends.js
@@ -0,0 +1,262 @@
+var friendsArray = [
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Jacob Deming",
+        "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        "scores": [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Jeremiah Scanlon",
+        "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Louis T. Delia",
+        "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        "scores": [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "Lou Ritter",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        "scores": [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Jordan Biason",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+        "scores": [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        "name": "Kane",
+        "photo": "Test",
+        "scores": [
+            "2",
+            "2",
+            "3",
+            "4",
+            "2",
+            "4",
+            "3",
+            "2",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Batman",
+        "photo": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/batman-begins-christian-bale-1554115577.jpg?crop=0.563xw:1.00xh;0.122xw,0&resize=3384:*",
+        "scores": [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        "name": "1",
+        "photo": "1",
+        "scores": [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        "name": "test",
+        "photo": "test",
+        "scores": [
+            "2",
+            "1",
+            "1",
+            "2",
+            "1",
+            "3",
+            "1",
+            "2",
+            "2",
+            "2"
+        ]
+    },
+    {
+        "name": "kira",
+        "photo": "akwlef",
+        "scores": [
+            "3",
+            "2",
+            "3",
+            "4",
+            "4",
+            "2",
+            "4",
+            "2",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "john",
+        "photo": "https://i.pinimg.com/originals/16/39/c8/1639c8e34dbd74c1b25148adcb9acae3.jpg",
+        "scores": [
+            "5",
+            "5",
+            "3",
+            "1",
+            "3",
+            "1",
+            "4",
+            "2",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "test",
+        "photo": "test",
+        "scores": [
+            "2",
+            "1",
+            "2",
+            "2",
+            "3",
+            "2",
+            "3",
+            "2",
+            "2",
+            "1"
+        ]
+    },
+    {
+        "name": "ben",
+        "photo": "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiFh-7q9v3hAhUKUt8KHbQ6BiAQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fflower%2F&psig=AOvVaw3qUxr3_eEPMM1-zzmjAUh7&ust=1556923682440747",
+        "scores": [
+            "2",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        "name": "Joe Bob",
+        "photo": "https://www.a-photo.com",
+        "scores": [
+            "3",
+            "4",
+            "5",
+            "1",
+            "4",
+            "2",
+            "4",
+            "2",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Nammo",
+        "photo": "https://images-na.ssl-images-amazon.com/images/I/91c5vXSKGOL.png",
+        "scores": [
+            "5",
+            "3",
+            "2",
+            "2",
+            "4",
+            "1",
+            "4",
+            "1",
+            "3",
+            "4"
+        ]
+    },
+    
+];
+
+
+module.exports = friendsArray;
