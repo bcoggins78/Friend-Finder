0814560bc7ae3176a1f58b73612007bb606ed76f
bcoggins78 <bcoggins78@gmail.com>
2019-05-05 17:36:05 -0400

    added profession to existing json

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,23 +1,9 @@
 var friendsArray = [
-    {
-        "name": "Bryant Coggins",
-        "photo": "https://bcoggins78.github.io/New-Portfolio/assets/media/bryant.png",
-        "scores": [
-            "5",
-            "4",
-            "1",
-            "3",
-            "1",
-            "2",
-            "4",
-            "2",
-            "3",
-            "2"
-        ]
-    },
+    
     {
         "name": "Jack O'Neill",
         "photo": "https://www.gateworld.net/wiki/images/8/8c/Oneilljack.jpg",
+        "profession": "Lieutenant General, USAF",
         "scores": [
             "2",
             "4",
@@ -34,6 +20,7 @@ var friendsArray = [
     {
         "name": "Roy Trenneman",
         "photo": "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters/p-the-it-crowd-chris-odowd.jpg",
+        "profession": "IT Support at Reynholm Industries",
         "scores": [
             "4",
             "3",
@@ -50,6 +37,7 @@ var friendsArray = [
     {
         "name": "Castiel",
         "photo": "http://2.bp.blogspot.com/-uw6yVofS9dA/UyrIyruV1bI/AAAAAAAAEGE/VaOlRdBLA8w/s1600/castiel+1.JPG",
+        "profession": "Angel of the Lord",
         "scores": [
             "1",
             "5",
@@ -66,6 +54,7 @@ var friendsArray = [
     {
         "name": "Crowley",
         "photo": "https://pmctvline2.files.wordpress.com/2017/05/supernatural-mark-sheppard1.jpg",
+        "profession": "King of Hell",
         "scores": [
             "5",
             "4",
@@ -82,6 +71,7 @@ var friendsArray = [
     {
         "name": "The Doctor",
         "photo": "https://upload.wikimedia.org/wikipedia/en/2/21/Tenth_Doctor_%28Doctor_Who%29.jpg",
+        "profession": "Time Lord",
         "scores": [
             "5",
             "3",
@@ -98,6 +88,7 @@ var friendsArray = [
     {
         "name": "Tony Soprano",
         "photo": "https://amp.businessinsider.com/images/51c2d8eeecad04f97c000030-320-240.jpg",
+        "profession": "Waste Management Consultant",
         "scores": [
             "3",
             "4",
@@ -114,6 +105,7 @@ var friendsArray = [
     {
         "name": "Ash Williams",
         "photo": "https://static.tvtropes.org/pmwiki/pub/images/williams_ashley_j.jpg",
+        "profession": "S-Mart Sales Associate",
         "scores": [
             "2",
             "4",
@@ -130,6 +122,7 @@ var friendsArray = [
     {
         "name": "Dexter Morgan",
         "photo": "https://c-8oqtgrjgwu46x24koikzx2etcpmgtx2eeqo.g00.ranker.com/g00/3_c-8yyy.tcpmgt.eqo_/c-8OQTGRJGWU46x24jvvrux3ax2fx2fkoikz.tcpmgt.eqox2fwugt_pqfg_koix2f65x2f072277x2fqtkikpcnx2ffgzvgt-oqticp-vx78-ejctcevgtu-rjqvq-w32x3fyx3d402x26jx3d402x26hkvx3detqrx26etqrx3dhcegux26sx3d72x26hox3drlrix26k32e.octmx3dkocig_$/$/$/$/$/$",
+        "profession": "Blood Spatter Analyst",
         "scores": [
             "3",
             "4",
@@ -146,6 +139,7 @@ var friendsArray = [
     {
         "name": "Walter White",
         "photo": "https://cdn3.movieweb.com/i/article/0TvMcG4es3dx98j6ltEqJaCVeo94ni/738:50/Breaking-Bad-Movie-Bryan-Cranston-Responds.jpg",
+        "profession": "High School Chemistry Teacher",
         "scores": [
             "4",
             "3",
@@ -162,6 +156,7 @@ var friendsArray = [
     {
         "name": "Jon Snow",
         "photo": "https://vignette.wikia.nocookie.net/gameofthrones/images/d/d3/JonSnowSeason8HB.jpg/revision/latest/scale-to-width-down/333?cb=20190401173347",
+        "profession": "Rightful Heir to the Iron Throne",
         "scores": [
             "2",
             "4",
@@ -178,6 +173,7 @@ var friendsArray = [
     {
         "name": "Dave Lister",
         "photo": "http://i.imgur.com/dVcIFen.jpg",
+        "profession": "Bum",
         "scores": [
             "4",
             "2",
@@ -194,6 +190,7 @@ var friendsArray = [
     {
         "name": "Michael Scarn",
         "photo": "https://vignette.wikia.nocookie.net/banthapedia/images/3/33/Scarn.jpg/revision/latest/scale-to-width-down/180?cb=20091021140521",
+        "profession": "Secret Agent",
         "scores": [
             "4",
             "2",
@@ -210,6 +207,7 @@ var friendsArray = [
     {
         "name": "Peter Venkman",
         "photo": "https://upload.wikimedia.org/wikipedia/en/5/51/Peter_GB1.jpg",
+        "profession": "Ghostbuster",
         "scores": [
             "3",
             "3",
@@ -226,6 +224,7 @@ var friendsArray = [
     {
         "name": "Jean Luc Picard",
         "photo": "https://vignette.wikia.nocookie.net/memoryalpha/images/b/bf/Jean-Luc_Picard%2C_2379.jpg/revision/latest/scale-to-width-down/350?cb=20180924193040&path-prefix=en",
+        "profession": "Commanding officer of the USS Enterprise-E",
         "scores": [
             "4",
             "4",
@@ -242,6 +241,7 @@ var friendsArray = [
     {
         "name": "Johnny Lawrence",
         "photo": "https://vignette.wikia.nocookie.net/thekaratekid/images/b/bc/Johnny_lawrence_2018.jpg/revision/latest/scale-to-width-down/310?cb=20180512085837",
+        "profession": "Sensei of the Cobra Kai Dojo",
         "scores": [
             "2",
             "4",
