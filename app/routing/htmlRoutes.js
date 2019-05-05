077b69ee716b665bcc00b3f77497f4a8fe90f27f
bcoggins78 <bcoggins78@gmail.com>
2019-05-05 02:41:58 -0400

    added path for handling 400 errors

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,13 +1,26 @@
 var path = require("path");
 
-module.exports = function(app) {
-      
-    app.get("/survey", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/survey.html"));
-    });
-      
-    app.get("/", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/home.html"));
-    });
+module.exports = function (app) {
 
-  };
+  app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+  app.get("*", function (req, res) {
+    var error404 = "<html>" +
+      "<body><h1>404 Not Found </h1>" +
+      "<p>The page you were looking for: " + req.url + " can not be found</p>" +
+      "</body></html>";
+
+    
+    res.status(404);
+
+    
+    res.send(error404);
+  });
+
+};
