e59c6e6ece06bd78681842b2c7735a289382007e
bcoggins78 <bcoggins78@gmail.com>
2019-05-05 02:42:21 -0400

    updated static public path

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use(express.static("public"));
+app.use(express.static("./app/public"));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-  });
+});
