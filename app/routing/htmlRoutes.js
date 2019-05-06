1c44e575abda50c69f9aa67a03e66f95366e7938
bcoggins78 <bcoggins78@gmail.com>
2019-05-05 22:06:56 -0400

    Added comments

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,18 +2,22 @@ var path = require("path");
 
 module.exports = function (app) {
 
+  // Route to the survey page
   app.get("/survey", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
+  // Route to the home page
   app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
-  app.get("/members", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+  // Route to the friends page
+  app.get("/friends", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/friends.html"));
   });
 
+  // Route to the error page if the path is not recognized
   app.get("*", function (req, res) {
     var error404 = "<html>" +
       "<body><h1>404 Not Found </h1>" +
